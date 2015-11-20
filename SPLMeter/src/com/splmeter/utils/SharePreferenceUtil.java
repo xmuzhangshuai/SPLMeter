e6a3c53bacdfbe4cc32839dcca2f74bb2d971978
zhangshuai <1207509737@qq.com>
2015-11-20 16:45:49 +0800

    SharePreferenceUtil
    Signed-off-by: zhangshuai <1207509737@qq.com>

--- SPLMeter/src/com/splmeter/utils/SharePreferenceUtil.java
+++ SPLMeter/src/com/splmeter/utils/SharePreferenceUtil.java
@@ -15,6 +15,7 @@ public class SharePreferenceUtil {
 	private SharedPreferences sp;
 	private SharedPreferences.Editor editor;
 	public static final String USE_COUNT = "count";// 记录软件使用次数
+	public static final String CONFIG = "config";//评价信息
 
 	public SharePreferenceUtil(Context context, String file) {
 		sp = context.getSharedPreferences(file, Context.MODE_PRIVATE);
@@ -31,4 +32,53 @@ public class SharePreferenceUtil {
 		editor.commit();
 	}
 
+	// 性别
+	public String getGender() {
+		return sp.getString("gender", "");
+	}
+
+	public void setGender(String u_gender) {
+		editor.putString("gender", u_gender);
+		editor.commit();
+	}
+
+	// 年龄段
+	public String getAgeGroup() {
+		return sp.getString("ageGroup", "");
+	}
+
+	public void setAgeGroup(String ageGroup) {
+		editor.putString("ageGroup", ageGroup);
+		editor.commit();
+	}
+
+	// 室内，室外
+	public String getInOutDoor() {
+		return sp.getString("in_out_door", "");
+	}
+
+	public void setInOutDoor(String in_out_door) {
+		editor.putString("in_out_door", in_out_door);
+		editor.commit();
+	}
+
+	// 校准
+	public int getCalibration() {
+		return sp.getInt("Calibration", 0);
+	}
+
+	public void setCalibration(int calibration) {
+		editor.putInt("Calibration", calibration);
+		editor.commit();
+	}
+
+	// 是否分享给科学家
+	public boolean getAutoShare() {
+		return sp.getBoolean("autoshare", true);
+	}
+
+	public void setAutoShare(boolean isAutoShare) {
+		editor.putBoolean("autoshare", isAutoShare);
+		editor.commit();
+	}
 }
